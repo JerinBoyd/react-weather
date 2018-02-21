13eeed6186eda96e258b31279ce1d93fac97fcb7
JerinBoyd <maxers@mac.com>
2018-02-21 18:15:30 -0500

    added min and max to lat and lon also validation for both to be numbers only

--- src/App.js
+++ src/App.js
@@ -2,6 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      lat: 0,
+      lon: 0
+    };
+    this.handleLatChange = this.handleLatChange.bind(this);
+    this.handleLonChange = this.handleLonChange.bind(this);
+    
+  }
+  handleLatChange(e){
+    this.setState({
+      lat: +e.target.value
+    })
+  }
+  handleLonChange(e){
+    this.setState({
+      lon: +e.target.value
+    })
+  }
   render() {
     return (
       <div>
@@ -10,11 +30,21 @@ class App extends Component {
         <form>
         <label>
           Latitude:
-          <input type="text" name="" id="" />
+          <input type="number" 
+          min="-90"
+          max="90"
+          onChange={(e) => this.handleLatChange(e)}
+          value={this.state.lat} 
+          required/>
         </label>
         <label>
           longitude:
-          <input type="text" name="" id="" />
+          <input type="number"  
+          min="-180"
+          max="180"
+          onChange={(e) => this.handleLonChange(e)} 
+          value={this.state.lon} 
+          required/>
         </label>
         <button type='submit'>Get the Weather!</button>
 
