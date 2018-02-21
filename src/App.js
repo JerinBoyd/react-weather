eaeece5d54620497ef29e14222d137ca9e6a9437
JerinBoyd <maxers@mac.com>
2018-02-21 18:22:31 -0500

    wired submision for form

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
     };
     this.handleLatChange = this.handleLatChange.bind(this);
     this.handleLonChange = this.handleLonChange.bind(this);
-    
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   handleLatChange(e){
     this.setState({
@@ -22,12 +22,17 @@ class App extends Component {
       lon: +e.target.value
     })
   }
+  handleSubmit(e){
+    e.preventDefault();
+    console.log('jerk');
+
+  }
   render() {
     return (
       <div>
         <h1>React-Weather</h1>
         <p>Enter your latitude and longitude to get local weather!</p>
-        <form>
+        <form onSubmit={(e) => this.handleSubmit(e)}>
         <label>
           Latitude:
           <input type="number" 
