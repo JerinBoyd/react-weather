31973768d299190b76ecf5674a88f7c3eda21fb0
JerinBoyd <maxers@mac.com>
2018-02-22 20:36:17 -0500

    added image folder and added image selection for respective weather

---
+++ client/src/images/index.js
@@ -0,0 +1,16 @@
+import rain from './rain.png';
+
+
+const selection = (name) => {
+    switch(name){
+        case "rain":
+           return rain;
+        default:
+           return rain;
+    }
+    if(name === 'rain') { 
+        return rain;
+    }
+    return rain;
+}
+export default selection;
